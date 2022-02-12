5479ff6714fb361a51ce785f330bdc8ef22bf05a
harshitha <harshithauk1500@gmail.com>
2022-02-13 00:15:00 +0530

    added a java program on operators

---
+++ src/operators.java
@@ -0,0 +1,28 @@
+public class operators {
+    /*
+    *Arithmatic +,-,*,/,%
+    * Bitwise << >>
+    * Relational
+    * Logical
+     */
+    public static void main(String[] args){
+        System.out.println("Arithmatic Operators");
+        int m=6,n=4;
+        int r1= m+n;
+        int r2 =m-n;
+        int r3 =m*n;
+        double r4 = (double) m/n;
+
+        int r5 = m%n;
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+
+//        ++n; //pre-increment
+//        n++; //post-increment
+        
+    }
+}
