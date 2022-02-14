a7173b76d26588c7e3bfeae949adfcd481390201
harshitha <harshithauk1500@gmail.com>
2022-02-14 23:21:09 +0530

    added files related to nested loops

---
+++ src/nested.java
@@ -0,0 +1,45 @@
+public class nested {
+    public static void main(String[] args){
+//      TO print:
+//        0 1 2 3 4
+//        0 1 2 3 4
+//        0 1 2 3 4
+//        0 1 2 3 4
+//        0 1 2 3 4
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+//        To print:
+//        1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+//        To print:
+//        A
+//        AB
+//        ABC
+//        ABCD
+//        ABCDE
+//        ABCDEF
+        for(int i=65;i<=(65+5);i++){
+            for(int j=65;j<=i;j++){
+
+                System.out.print((char)j);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
