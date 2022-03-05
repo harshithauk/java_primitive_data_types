39bf404d620bc03b9c269f39c0d31d8c218df49c
harshitha <harshithauk1500@gmail.com>
2022-03-05 11:54:18 +0530

    added files related to super method and method overriding

---
+++ src/OverridingDemo.java
@@ -0,0 +1,17 @@
+class A1{
+    public void show(){
+        System.out.println("In A");
+    }
+}
+class B1 extends A1{
+    @Override
+    public void show(){
+        System.out.println("In B");
+    }
+}
+public class OverridingDemo {
+    public static void main(String[] args){
+        B1 obj1 = new B1();
+        obj1.show();
+    }
+}
